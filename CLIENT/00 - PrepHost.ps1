249e7518c4990b74516134abb780f34f02cf91ef
Cengiz Tuztas(ctuztas) <cengiz.tuztas@gmx.de>
2023-12-08 21:19:04 +0100

    Initial setup

---
+++ CLIENT/00 - PrepHost.ps1
@@ -0,0 +1,7 @@
+# Prep Host Requirements
+Install-WindowsFeature NET-Framework-Core 
+
+# Download Install WinGet
+Invoke-WebRequest -Uri “https://github.com/microsoft/winget-cli/releases/download/v1.6.3133/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle” -OutFile “C:\Temp\WinGet.msixbundle”
+# Install WinGet
+Add-AppxPackage “C:\Temp\WinGet.msixbundle”

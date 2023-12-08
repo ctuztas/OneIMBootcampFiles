e1e3504e515676937d27d47a5f3d2e2c165e21d6
Cengiz Tuztas(ctuztas) <cengiz.tuztas@gmx.de>
2023-12-08 23:59:35 +0100

    updated scripts

--- DC1/00 - PrepHost.ps1
+++
@@ -1,5 +0,0 @@
-# Prep Host Requirements
-Install-WindowsFeature NET-Framework-Core AD-Domain-Services -IncludeManagementTools
-
-# Prep Active Directory
-Install-ADDSForest -DomainName "iamdemo.com" -DomainMode "Win2012R2" -ForestMode "Win2012R2" -InstallDNS -Confirm -SafeModeAdministratorPassword (ConvertTo-SecureString -AsPlainText "Kub1Lay$" -Force) 

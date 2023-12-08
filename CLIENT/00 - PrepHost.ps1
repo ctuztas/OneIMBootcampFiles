8a0322688dd5d4d622235f6cc88ef73d86ac5880
Cengiz Tuztas(ctuztas) <cengiz.tuztas@gmx.de>
2023-12-08 22:04:30 +0100

    Fixed typos

--- CLIENT/00 - PrepHost.ps1
+++ CLIENT/00 - PrepHost.ps1
@@ -2,6 +2,6 @@
 Install-WindowsFeature NET-Framework-Core 
 
 # Download Install WinGet
-Invoke-WebRequest -Uri “https://github.com/microsoft/winget-cli/releases/download/v1.6.3133/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle” -OutFile “C:\Temp\WinGet.msixbundle”
+Invoke-WebRequest -Uri "https://github.com/microsoft/winget-cli/releases/download/v1.6.3133/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -OutFile "C:\Temp\WinGet.msixbundle"
 # Install WinGet
-Add-AppxPackage “C:\Temp\WinGet.msixbundle”
+Add-AppxPackage "C:\Temp\WinGet.msixbundle"
